cb0eb22a68c6b06f96dcad16c946aef348dcbad8
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2020-06-08 23:06:11 -0700

    generate port list with busInterface annotations

--- test/basic.js
+++ test/basic.js
@@ -16,30 +16,30 @@ describe('basic', () => {
 module noname (
 
 );
-endmodule
+endmodule // noname
 `);
   });
 
   it('b1', async () => {
     expect(generate({
       component: {
-        name: 'simple',
+        name: 'b1',
         model: {ports: {clock: 1, reset_n: 1, irq: -1}}
       }
 })).to.eq(`\
-module simple (
-  input                                      clock,
-  input                                      reset_n,
-  output logic                               irq
+module b1 (
+  input   clock,
+  input   reset_n,
+  output  irq
 );
-endmodule
+endmodule // b1
 `);
   });
 
   it('b2', async () => {
     expect(generate({
       component: {
-        name: 'simple',
+        name: 'bar',
         model: {
           ports: {
             clock: 1, reset_n: 1,
@@ -48,14 +48,48 @@ endmodule
         }
       }
 })).to.eq(`\
-module simple (
-  input                                      clock,
-  input                                      reset_n,
-  input                                      t_req,
-  output logic                               t_ack,
-  output logic                [datWidth-1:0] t_dat
+module bar (
+  input                 clock,
+  input                 reset_n,
+  input                 t_req,
+  output                t_ack,
+  input  [datWidth-1:0] t_dat
 );
-endmodule
+endmodule // bar
+`);
+  });
+
+  it('b3', async () => {
+    expect(generate({
+      component: {
+        name: 'b3',
+        model: {ports: {
+          clk: 1, wren: 1, rden: 1, addr: 16, wrdata: 'dataWidth', rddata: '-dataWidth'
+        }},
+        busInterfaces: [{
+          name: 't',
+          interfaceMode: 'slave',
+          busType: {vendor: 'sifive.com', library: 'MEM', name: 'SPRAM', version: '0.1.0'},
+          abstractionTypes: [{
+            viewRef: 'RTLview',
+            portMaps: {
+              CLK: 'clk', WREN: 'wren', RDEN: 'rden', ADDR: 'addr',
+              WRDATA: 'wrdata', RDDATA: 'rddata'
+            }
+          }]
+        }],
+        props: {}
+      }
+    })).to.eq(`\
+module b3 (
+  input                  clk    /* t:CLK */,
+  input                  wren   /* t:WREN */,
+  input                  rden   /* t:RDEN */,
+  input           [15:0] addr   /* t:ADDR */,
+  input  [dataWidth-1:0] wrdata /* t:WRDATA */,
+  output [dataWidth-1:0] rddata /* t:RDDATA */
+);
+endmodule // b3
 `);
   });
 
